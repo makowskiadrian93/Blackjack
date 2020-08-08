85d32f265016b2fd5316bacd18b8bd2e54124714
Adrian Von Makowski <makowski.adrian.93@gmail.com>
2020-08-08 15:02:03 +0200

    15:02 08.08.2020 fixed

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,54 @@
 
 <template>
   <div>
-    <BlackJackTitle></BlackJackTitle>
+    <div class="title">
+      <section>
+        <h1>BLACKJACK</h1>
+        <div>
+          <button @click="singlePlayer">POJEDYŃCZY GRACZ</button>
+          <!-- <button @click="multiPlayer">DWÓCH GRACZY</button> -->
+          <h2 class="footer">♠ ♥ ♦ ♣</h2>
+        </div>
+      </section>
+    </div>
     <SinglePlayer v-if="singlePlayerMode"></SinglePlayer>
   </div>
 </template>
 
 
 <script>
-import BlackJackTitle from "./components/BlackJackTitle.vue";
 import SinglePlayer from "./components/SinglePlayer.vue";
 
 export default {
   data: function() {
     return {
-      singlePlayerMode: false,
+      singlePlayerMode: false
     };
   },
   components: {
-   BlackJackTitle,
-   SinglePlayer
+    SinglePlayer
   },
-  methods: {
+   methods: {
     singlePlayer() {
-      this.SinglePlayerMode = true;
+      console.log('rozpoczynamy singlePlayera');
+      this.singlePlayerMode = true;
     }
   },
-  
 };
 </script>
 <style>
 body,
 html {
   background-color: green;
+}
+.title {
+  display: grid;
+  grid-template-columns: auto;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 2rem;
+  text-align: center;
+}
+.footer {
+  text-align: center;
 }
 </style>
